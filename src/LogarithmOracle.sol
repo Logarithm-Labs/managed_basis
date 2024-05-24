dc71cba558b0c928e459f5a62612a575cc6604f7
TalCrypto <leehunter936@gmail.com>
2024-05-24 10:38:59 +0800

    feat: upgradeable oracle

--- src/LogarithmOracle.sol
+++ src/LogarithmOracle.sol
@@ -3,52 +3,92 @@ pragma solidity ^0.8.0;
 
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
-import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Ownable2StepUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 
 import {IPriceFeed} from "src/externals/chainlink/interfaces/IPriceFeed.sol";
 
 import {IOracle} from "src/interfaces/IOracle.sol";
 import {Errors} from "src/libraries/Errors.sol";
 
-contract LogarithmOracle is IOracle, Ownable2Step {
+contract LogarithmOracle is IOracle, UUPSUpgradeable, Ownable2StepUpgradeable {
     uint256 public constant FLOAT_PRECISION = 1e30;
 
-    mapping(address asset => IPriceFeed) public priceFeeds;
-    mapping(address priceFeed => uint256) public heartbeatDurations;
+    /*//////////////////////////////////////////////////////////////
+                        NAMESPACED STORAGE LAYOUT
+    //////////////////////////////////////////////////////////////*/
 
-    event PriceFeedUpdated(address asset, address feed, uint256 heartbeatDuration);
+    /// @custom:storage-location erc7201:logarithm.storage.LogarithmOracle
+    struct LogarithmOracleStorage {
+        mapping(address asset => IPriceFeed) priceFeeds;
+        mapping(address priceFeed => uint256) heartbeatDurations;
+    }
 
-    constructor() Ownable(msg.sender) {}
+    // keccak256(abi.encode(uint256(keccak256("logarithm.storage.LogarithmOracle")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 private constant LogarithmOracleStorageLocation =
+        0x2c93e30cf348944d3203d68cfbfc99aa913023170c5de2049def7c26b25c6400;
+
+    function _getLogarithmOracleStorage() private pure returns (LogarithmOracleStorage storage $) {
+        assembly {
+            $.slot := LogarithmOracleStorageLocation
+        }
+    }
+
+    event PriceFeedUpdated(address asset, address feed);
+    event HeartBeatUpdated(address asset, uint256 heartbeatDuration);
+
+    function initialize(address owner_) external initializer {
+        __Ownable_init(owner_);
+    }
+
+    function _authorizeUpgrade(address /*newImplementation*/ ) internal virtual override onlyOwner {}
 
     function renounceOwnership() public pure override {
         revert();
     }
 
-    function setPriceFeeds(address[] calldata assets, address[] calldata feeds, uint256[] calldata heartbeats)
+    function setPriceFeeds(address[] calldata assets, address[] calldata feeds) external onlyOwner {
+        uint256 len = assets.length;
+        if (len != feeds.length) {
+            revert Errors.IncosistentParamsLength();
+        }
+        for (uint256 i; i < len;) {
+            _getLogarithmOracleStorage().priceFeeds[assets[i]] = IPriceFeed(feeds[i]);
+            unchecked {
+                ++i;
+            }
+            emit PriceFeedUpdated(assets[i], feeds[i]);
+        }
+    }
+
+    function setHeartbeats(address[] calldata assets, address[] calldata feeds, uint256[] calldata heartbeats)
         external
         onlyOwner
     {
         uint256 len = assets.length;
-        if (len != feeds.length || len != heartbeats.length) {
+        if (len != heartbeats.length) {
             revert Errors.IncosistentParamsLength();
         }
         for (uint256 i; i < len;) {
-            priceFeeds[assets[i]] = IPriceFeed(feeds[i]);
-            heartbeatDurations[feeds[i]] = heartbeats[i];
+            _getLogarithmOracleStorage().heartbeatDurations[feeds[i]] = heartbeats[i];
             unchecked {
                 ++i;
             }
-            emit PriceFeedUpdated(assets[i], feeds[i], heartbeats[i]);
+            emit HeartBeatUpdated(assets[i], heartbeats[i]);
         }
+    }
+
+    function getPriceFeed(address asset) external view returns (address) {
+        return address(_getLogarithmOracleStorage().priceFeeds[asset]);
     }
 
     /// @dev returns token price in (30 - decimal of token)
     /// so that the usd value of token has 30 decimals
     /// for example, if usdc has 6 decimals, then this returns its price in 30 - 6 = 24 decimals
     function getAssetPrice(address asset) external view override returns (uint256) {
-        IPriceFeed priceFeed = priceFeeds[asset];
+        IPriceFeed priceFeed = _getLogarithmOracleStorage().priceFeeds[asset];
 
         if (address(priceFeed) == address(0)) {
             revert Errors.PriceFeedNotConfigured();
@@ -69,7 +109,7 @@ contract LogarithmOracle is IOracle, Ownable2Step {
         }
 
         // in case chainlink price feeds are not updated
-        uint256 heartbeatDuration = heartbeatDurations[address(priceFeed)];
+        uint256 heartbeatDuration = _getLogarithmOracleStorage().heartbeatDurations[address(priceFeed)];
         if (block.timestamp > timestamp && block.timestamp - timestamp > heartbeatDuration) {
             revert Errors.PriceFeedNotUpdated(asset, timestamp, heartbeatDuration);
         }
