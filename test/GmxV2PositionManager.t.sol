b12a33de1b91ba2af81378a6c0e62e3470dcd3a5
TalCrypto <leehunter936@gmail.com>
2024-05-28 15:38:08 +0800

    chore: vscode integration

---
+++ test/GmxV2PositionManager.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {console} from "forge-std/console.sol";
+
+contract GmxV2PositionManager is StdInvariant, Test {
+    function setUp() public {}
+
+    function test_() public {}
+
+    function invariant_() public {}
+}
