47910c11a5e3fba23512abfe1d93c8acf385ee63
Numa <numapumpilius@proton.me>
2024-07-25 00:30:40 +0300

    deploy.md

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -12,7 +12,7 @@ import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.s
 contract DeployScript is Script {
     AccumulatedBasisStrategy public strategy;
     OffChainPositionManager public positionManager;
-    LogarithmOracle public oracle;
+    LogarithmOracle public oracle = LogarithmOracle(0x949C9908Cd6C2F0Bca623355f4C5BaF157c3fb70);
 
     address public operator = 0x78057a43dDc57792340BC19E50e1011F8DAdEd01;
     address public agent = 0xA2a7e3a770c38aAe24F175a38281f74731Fe477E;
@@ -33,26 +33,26 @@ contract DeployScript is Script {
     function run() public {
         vm.startBroadcast();
 
-        // deploy oracle
-        address oracleImpl = address(new LogarithmOracle());
-        address oracleProxy =
-            address(new ERC1967Proxy(oracleImpl, abi.encodeWithSelector(LogarithmOracle.initialize.selector, owner)));
-        oracle = LogarithmOracle(oracleProxy);
-        require(oracle.owner() == owner, "DeployScript: oracle owner mismatch");
-        console.log("Oracle deployed at: %s", address(oracle));
+        // // deploy oracle
+        // address oracleImpl = address(new LogarithmOracle());
+        // address oracleProxy =
+        //     address(new ERC1967Proxy(oracleImpl, abi.encodeWithSelector(LogarithmOracle.initialize.selector, owner)));
+        // oracle = LogarithmOracle(oracleProxy);
+        // require(oracle.owner() == owner, "DeployScript: oracle owner mismatch");
+        // console.log("Oracle deployed at: %s", address(oracle));
 
-        // set oracle price feed
-        address[] memory assets = new address[](2);
-        address[] memory feeds = new address[](2);
-        uint256[] memory heartbeats = new uint256[](2);
-        assets[0] = asset;
-        assets[1] = product;
-        feeds[0] = assetPriceFeed;
-        feeds[1] = productPriceFeed;
-        heartbeats[0] = 24 * 3600;
-        heartbeats[1] = 24 * 3600;
-        oracle.setPriceFeeds(assets, feeds);
-        oracle.setHeartbeats(feeds, heartbeats);
+        // // set oracle price feed
+        // address[] memory assets = new address[](2);
+        // address[] memory feeds = new address[](2);
+        // uint256[] memory heartbeats = new uint256[](2);
+        // assets[0] = asset;
+        // assets[1] = product;
+        // feeds[0] = assetPriceFeed;
+        // feeds[1] = productPriceFeed;
+        // heartbeats[0] = 24 * 3600;
+        // heartbeats[1] = 24 * 3600;
+        // oracle.setPriceFeeds(assets, feeds);
+        // oracle.setHeartbeats(feeds, heartbeats);
 
         // deploy strategy
         address strategyImpl = address(new AccumulatedBasisStrategy());
