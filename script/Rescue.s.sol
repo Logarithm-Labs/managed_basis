746ea3da8abaac84e35a363bb8f6c30a143a6644
Numa <numapumpilius@proton.me>
2024-07-25 13:25:48 +0300

    Merge script/Rescue.s.sol from pruned-strategy branch

---
+++ script/Rescue.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ManagedBasisStrategyPruned} from "src/ManagedBasisStrategyPruned.sol";
+import {RescueStrategy} from "src/RescueStrategy.sol";
+
+contract RescueScript is Script {
+    ManagedBasisStrategyPruned public strategy = ManagedBasisStrategyPruned(0x8BAAB907986EB5d34E11b1a128F38e5085a9a24f);
+    address public keeper = 0x4F42fa2f07f81e6E1D348245EcB7EbFfC5267bE0;
+    address public usdc = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+
+    function run() public {
+        vm.startBroadcast();
+        address impl = address(new RescueStrategy());
+        uint256 balStrategy = IERC20(usdc).balanceOf(address(strategy));
+        uint256 balBefore = IERC20(usdc).balanceOf(address(keeper));
+        strategy.upgradeToAndCall(impl, abi.encodeWithSelector(RescueStrategy.rescue.selector, ""));
+        uint256 balAfter = IERC20(usdc).balanceOf(address(keeper));
+        require(balAfter - balBefore == balStrategy, "RescueScript: rescue failed");
+    }
+}
