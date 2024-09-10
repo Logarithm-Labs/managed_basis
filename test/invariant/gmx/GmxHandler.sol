5e3767b13cb7e3757a785d4339b6bb6467a85aac
TalCrypto <leehunter936@gmail.com>
2024-09-10 17:48:16 +0800

    test: invariant

---
+++ test/invariant/gmx/GmxHandler.sol
@@ -0,0 +1,168 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import {BeaconProxy} from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
+
+import {IPriceFeed} from "src/externals/chainlink/interfaces/IPriceFeed.sol";
+import {BasisStrategy} from "src/BasisStrategy.sol";
+import {LogarithmVault} from "src/LogarithmVault.sol";
+import {LogarithmOracle} from "src/LogarithmOracle.sol";
+import {GmxConfig} from "src/GmxConfig.sol";
+import {GmxGasStation} from "src/GmxGasStation.sol";
+import {GmxV2PositionManager} from "src/GmxV2PositionManager.sol";
+
+import {GmxV2Test} from "test/base/GmxV2Test.sol";
+import {ForkTest} from "test/base/ForkTest.sol";
+import {StrategyHelper, StrategyState} from "test/helper/StrategyHelper.sol";
+
+contract GmxHandler is GmxV2Test {
+    BasisStrategy strategy;
+    LogarithmVault vault;
+    LogarithmOracle oracle;
+
+    StrategyHelper helper;
+
+    address owner;
+    address operator;
+    address forwarder;
+
+    IERC20 asset;
+    IERC20 product;
+
+    address[] public actors;
+
+    address internal currentActor;
+
+    modifier useActor(uint256 actorIndexSeed) {
+        uint256 len = actors.length;
+        if (len == 0) currentActor = msg.sender;
+        else currentActor = actors[bound(actorIndexSeed, 0, len - 1)];
+
+        vm.startPrank(currentActor);
+        _;
+        vm.stopPrank();
+    }
+
+    constructor(BasisStrategy _strategy, address _owner, address _operator, address _forwarder) {
+        owner = _owner;
+        operator = _operator;
+        forwarder = _forwarder;
+        strategy = _strategy;
+        asset = IERC20(_strategy.asset());
+        product = IERC20(_strategy.product());
+        vault = LogarithmVault(_strategy.vault());
+        oracle = LogarithmOracle(_strategy.oracle());
+        address positionManagerAddr = _initPositionManager(owner, address(_strategy));
+        vm.startPrank(owner);
+        strategy.setPositionManager(positionManagerAddr);
+        vm.stopPrank();
+
+        helper = new StrategyHelper(address(strategy));
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                               USER LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function deposit(uint256 assets) public {
+        address user = msg.sender;
+        assets = bound(assets, 0, vault.maxDeposit(user));
+        assets = bound(assets, 0, asset.balanceOf(USDC_WHALE));
+
+        actors.push(user);
+
+        vm.startPrank(USDC_WHALE);
+        asset.transfer(user, assets);
+
+        vm.startPrank(user);
+        asset.approve(address(vault), assets);
+        vault.deposit(assets, user);
+        vm.stopPrank();
+    }
+
+    function redeem(uint256 shares, uint256 actorIndexSeed) public useActor(actorIndexSeed) {
+        shares = bound(shares, 0, vault.balanceOf(currentActor));
+        vault.redeem(shares, currentActor, currentActor);
+    }
+
+    function claim(uint256 actorIndexSeed) public useActor(actorIndexSeed) {
+        uint256 nonce = vault.nonces(currentActor);
+        for (uint256 i; i < nonce; i++) {
+            bytes32 key = vault.getWithdrawKey(currentActor, nonce);
+            if (vault.isClaimable(key)) {
+                vault.claim(key);
+            }
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             OPERATOR LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function utilize(uint256 amount) public {
+        (uint256 utilization,) = strategy.pendingUtilizations();
+        if (utilization == 0) return;
+        amount = bound(amount, 1, utilization);
+        vm.startPrank(operator);
+        strategy.utilize(amount, BasisStrategy.SwapType.MANUAL, "");
+    }
+
+    function deutilize(uint256 amount) public {
+        (, uint256 deutilization) = strategy.pendingUtilizations();
+        if (deutilization == 0) return;
+        amount = bound(amount, 1, deutilization);
+        vm.startPrank(operator);
+        strategy.deutilize(amount, BasisStrategy.SwapType.MANUAL, "");
+    }
+
+    function perforUpkeep() public {
+        (bool upkeepNeeded, bytes memory performData) = strategy.checkUpkeep("");
+        if (upkeepNeeded) {
+            strategy.performUpkeep(performData);
+        }
+    }
+
+    function executeOrder() public {
+        _excuteOrder();
+    }
+
+    function fullExecution() public {
+        perforUpkeep();
+        (uint256 utilization, uint256 deutilization) = strategy.pendingUtilizations();
+        if (utilization > 0) {
+            utilize(utilization);
+            executeOrder();
+        }
+        if (deutilization > 0) {
+            deutilize(deutilization);
+            executeOrder();
+        }
+        perforUpkeep();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                  MOCK
+    //////////////////////////////////////////////////////////////*/
+
+    function updateProductPrice(bool isRise, bool isProduct, uint256 fluctuation) public {
+        fluctuation = bound(fluctuation, 0, 0.5 ether);
+        if (fluctuation == 0) return;
+
+        address priceFeed = isProduct ? oracle.getPriceFeed(address(product)) : oracle.getPriceFeed(address(asset));
+        int256 currPrice = IPriceFeed(priceFeed).latestAnswer();
+        uint256 deltaPrice = Math.mulDiv(uint256(currPrice), fluctuation, 1 ether);
+
+        int256 resultedPrice = isRise ? currPrice + int256(deltaPrice) : currPrice - int256(deltaPrice);
+        _mockChainlinkPriceFeedAnswer(priceFeed, resultedPrice);
+    }
+
+    function callSummary() public view {
+        StrategyState memory state = helper.getStrategyState();
+        helper.logStrategyState("SUMMARY", state);
+    }
+}
---
+++ test/invariant/offchain/OffChainInvariants.sol
@@ -0,0 +1,164 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import {BeaconProxy} from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {Test} from "forge-std/Test.sol";
+
+import {IPriceFeed} from "src/externals/chainlink/interfaces/IPriceFeed.sol";
+
+import {ForkTest} from "test/base/ForkTest.sol";
+import {MockPriceFeed} from "test/mock/MockPriceFeed.sol";
+
+import {IPositionManager} from "src/interfaces/IPositionManager.sol";
+import {LogarithmOracle} from "src/LogarithmOracle.sol";
+import {LogarithmVault} from "src/LogarithmVault.sol";
+import {StrategyConfig} from "src/StrategyConfig.sol";
+import {BasisStrategy} from "src/BasisStrategy.sol";
+
+import {OffChainHandler} from "./OffChainHandler.sol";
+
+contract OffChainInvariants is StdInvariant, ForkTest {
+    address owner = makeAddr("owner");
+    address operator = makeAddr("operator");
+    address forwarder = makeAddr("forwarder");
+
+    address constant asset = USDC; // USDC
+    address constant product = WETH; // WETH
+    address constant assetPriceFeed = 0x50834F3163758fcC1Df9973b6e91f0F0F0434aD3; // Chainlink USDC-USD price feed
+    address constant productPriceFeed = 0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612; // Chainlink ETH-USD price feed
+    uint256 constant entryCost = 0.01 ether;
+    uint256 constant exitCost = 0.02 ether;
+    bool constant isLong = false;
+    uint256 constant targetLeverage = 3 ether;
+    uint256 constant minLeverage = 2 ether;
+    uint256 constant maxLeverage = 5 ether;
+    uint256 constant safeMarginLeverage = 20 ether;
+
+    uint256 constant MAX_DEPOSIT = 100_000_000 * 1e6;
+
+    BasisStrategy strategy;
+    LogarithmVault vault;
+
+    OffChainHandler handler;
+
+    function setUp() public {
+        _forkArbitrum(0);
+
+        vm.startPrank(owner);
+        // deploy oracle
+        address oracleImpl = address(new LogarithmOracle());
+        address oracleProxy =
+            address(new ERC1967Proxy(oracleImpl, abi.encodeWithSelector(LogarithmOracle.initialize.selector, owner)));
+        LogarithmOracle oracle = LogarithmOracle(oracleProxy);
+        vm.label(address(oracle), "oracle");
+
+        // mock uniswap
+        _mockUniswapPool(UNISWAPV3_WETH_USDC, oracleProxy);
+
+        // set oracle price feed
+        address[] memory assets = new address[](2);
+        address[] memory feeds = new address[](2);
+        uint256[] memory heartbeats = new uint256[](2);
+        assets[0] = asset;
+        assets[1] = product;
+        feeds[0] = assetPriceFeed;
+        feeds[1] = productPriceFeed;
+        heartbeats[0] = 24 * 3600;
+        heartbeats[1] = 24 * 3600;
+        oracle.setPriceFeeds(assets, feeds);
+        oracle.setHeartbeats(feeds, heartbeats);
+        // _mockChainlinkPriceFeed(assetPriceFeed);
+        address mockPriceFeed = address(new MockPriceFeed());
+
+        (uint80 roundID, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) =
+            IPriceFeed(assetPriceFeed).latestRoundData();
+        uint8 decimals = IPriceFeed(assetPriceFeed).decimals();
+        vm.etch(assetPriceFeed, mockPriceFeed.code);
+        MockPriceFeed(assetPriceFeed).setOracleData(roundID, answer, startedAt, updatedAt, answeredInRound, decimals);
+
+        // _mockChainlinkPriceFeed(productPriceFeed);
+        (roundID, answer, startedAt, updatedAt, answeredInRound) = IPriceFeed(productPriceFeed).latestRoundData();
+        decimals = IPriceFeed(productPriceFeed).decimals();
+        vm.etch(productPriceFeed, mockPriceFeed.code);
+        MockPriceFeed(productPriceFeed).setOracleData(roundID, answer, startedAt, updatedAt, answeredInRound, decimals);
+
+        address[] memory pathWeth = new address[](3);
+        pathWeth[0] = USDC;
+        pathWeth[1] = UNISWAPV3_WETH_USDC;
+        pathWeth[2] = WETH;
+
+        address vaultImpl = address(new LogarithmVault());
+        address vaultProxy = address(
+            new ERC1967Proxy(
+                vaultImpl,
+                abi.encodeWithSelector(
+                    LogarithmVault.initialize.selector, owner, asset, entryCost, exitCost, "tt", "tt"
+                )
+            )
+        );
+        vault = LogarithmVault(vaultProxy);
+        vm.label(address(vault), "vault");
+
+        StrategyConfig config = new StrategyConfig();
+        config.initialize(owner);
+
+        // deploy strategy
+        address strategyImpl = address(new BasisStrategy());
+        address strategyProxy = address(
+            new ERC1967Proxy(
+                strategyImpl,
+                abi.encodeWithSelector(
+                    BasisStrategy.initialize.selector,
+                    address(config),
+                    product,
+                    address(vault),
+                    oracle,
+                    operator,
+                    targetLeverage,
+                    minLeverage,
+                    maxLeverage,
+                    safeMarginLeverage,
+                    pathWeth
+                )
+            )
+        );
+        strategy = BasisStrategy(strategyProxy);
+        strategy.setForwarder(forwarder);
+        vm.label(address(strategy), "strategy");
+
+        vault.setStrategy(address(strategy));
+        vault.setDepositLimits(MAX_DEPOSIT, MAX_DEPOSIT);
+        vm.stopPrank();
+
+        handler = new OffChainHandler(strategy, owner, operator, forwarder);
+
+        targetContract(address(handler));
+    }
+
+    function invariant_sharePriceShouldBeAroundOne() public view {
+        uint256 totalAssets = vault.totalAssets();
+        uint256 totalSupply = vault.totalSupply();
+        if (totalSupply == 0) return;
+        uint256 sharePrice = Math.mulDiv(totalAssets, 1e6, totalSupply);
+        assertApproxEqRel(sharePrice, 1e6, 0.01 ether, "share price");
+    }
+
+    function invariant_leverageShouldBeBetweenMinAndMaxWhenUpkeepNotNeeded() public view {
+        (bool upkeepNeeded,) = strategy.checkUpkeep("");
+        if (!upkeepNeeded) {
+            uint256 currLeverage = IPositionManager(strategy.positionManager()).currentLeverage();
+            assertTrue(currLeverage >= minLeverage, "minLeverage");
+            assertTrue(currLeverage <= maxLeverage, "maxLeverage");
+        }
+    }
+
+    function invariant_utilizationAmountsShouldBeExclusive() public view {
+        (uint256 utilization, uint256 deutilization) = strategy.pendingUtilizations();
+        assertFalse(utilization > 0 && deutilization > 0, "exclusive");
+    }
+}
