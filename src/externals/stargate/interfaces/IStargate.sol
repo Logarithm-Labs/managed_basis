1773113bbfb076f487ff70df45ab6fbcf1f2dc26
TalCrypto <leehunter936@gmail.com>
2024-11-22 01:32:55 +0800

    chore: forge fmt

--- src/externals/stargate/interfaces/IStargate.sol
+++ src/externals/stargate/interfaces/IStargate.sol
@@ -3,7 +3,13 @@ pragma solidity ^0.8.0;
 
 // Solidity does not support splitting import across multiple lines
 // solhint-disable-next-line max-line-length
-import { IOFT, SendParam, MessagingFee, MessagingReceipt, OFTReceipt } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/interfaces/IOFT.sol";
+import {
+    IOFT,
+    SendParam,
+    MessagingFee,
+    MessagingReceipt,
+    OFTReceipt
+} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/interfaces/IOFT.sol";
 
 /// @notice Stargate implementation type.
 enum StargateType {
@@ -22,11 +28,10 @@ struct Ticket {
 interface IStargate is IOFT {
     /// @dev This function is same as `send` in OFT interface but returns the ticket data if in the bus ride mode,
     /// which allows the caller to ride and drive the bus in the same transaction.
-    function sendToken(
-        SendParam calldata _sendParam,
-        MessagingFee calldata _fee,
-        address _refundAddress
-    ) external payable returns (MessagingReceipt memory msgReceipt, OFTReceipt memory oftReceipt, Ticket memory ticket);
+    function sendToken(SendParam calldata _sendParam, MessagingFee calldata _fee, address _refundAddress)
+        external
+        payable
+        returns (MessagingReceipt memory msgReceipt, OFTReceipt memory oftReceipt, Ticket memory ticket);
 
     /// @notice Returns the Stargate implementation type.
     function stargateType() external pure returns (StargateType);
