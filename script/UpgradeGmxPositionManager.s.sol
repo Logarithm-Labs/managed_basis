74959ddbbbc7a30225fee4bf65211700b876f31a
TalCrypto <leehunter936@gmail.com>
2024-11-28 18:43:29 +0800

    deploy: upgrade gmx

--- script/UpgradeGmxPositionManager.s.sol
+++ script/UpgradeGmxPositionManager.s.sol
@@ -6,14 +6,18 @@ import {GmxV2PositionManager} from "src/hedge/gmx/GmxV2PositionManager.sol";
 import {GasStation} from "src/gas-station/GasStation.sol";
 import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 import {Arbitrum} from "script/utils/ProtocolAddresses.sol";
+import {ArbiAddresses} from "script/utils/ArbiAddresses.sol";
+import {GmxConfig} from "src/hedge/gmx/GmxConfig.sol";
 
 contract UpgradeGmxPositionManagerScript is Script {
-    UpgradeableBeacon positionManagerBeacon = UpgradeableBeacon(0x91544E205446E673aeC904c53BdB7cA9b892CD5E);
-    GmxV2PositionManager positionManager = GmxV2PositionManager(0x5903078b87795b85388102E0881d545C0f36E231);
+    UpgradeableBeacon beaconGmx = UpgradeableBeacon(Arbitrum.BEACON_GMX_POSITION_MANAGER);
+    GmxConfig gmxConfig = GmxConfig(Arbitrum.CONFIG_GMX);
 
     function run() public {
         vm.startBroadcast();
-        address positionManagerImpl = address(new GmxV2PositionManager());
-        positionManagerBeacon.upgradeTo(positionManagerImpl);
+        address newImpl = address(new GmxV2PositionManager());
+        beaconGmx.upgradeTo(newImpl);
+        gmxConfig.updateAddresses(ArbiAddresses.GMX_EXCHANGE_ROUTER, ArbiAddresses.GMX_READER);
+        vm.stopBroadcast();
     }
 }
