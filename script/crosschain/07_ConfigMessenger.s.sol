729fcd539959273743c715397790bccd7a8511d0
TalCrypto <leehunter936@gmail.com>
2024-12-09 13:30:28 +0800

    chore: config messengers

---
+++ script/crosschain/07_ConfigMessenger.s.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import {XSpotManager} from "src/spot/crosschain/XSpotManager.sol";
+import {Arbitrum, Bsc} from "script/utils/ProtocolAddresses.sol";
+import {ArbiAddresses} from "script/utils/ArbiAddresses.sol";
+import {BscAddresses} from "script/utils/BscAddresses.sol";
+import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import {DeployHelper} from "script/utils/DeployHelper.sol";
+import {GasStation} from "src/gas-station/GasStation.sol";
+import {BrotherSwapper} from "src/spot/crosschain/BrotherSwapper.sol";
+import {AddressCast} from "src/libraries/utils/AddressCast.sol";
+import {ILogarithmMessenger} from "src/messenger/ILogarithmMessenger.sol";
+
+contract ArbConfigScript is Script {
+    // access control addresses
+    address constant owner = 0xDaFed9a0A40f810FCb5C3dfCD0cB3486036414eb;
+    uint256 constant BSC_CHAIN_ID = 56;
+    uint32 constant BSC_EID = uint32(30102);
+    ILogarithmMessenger messenger = ILogarithmMessenger(ArbiAddresses.LOGARITHM_MESSENGER);
+
+    function run() public {
+        vm.startBroadcast();
+
+        messenger.authorize(Arbitrum.X_SPOT_MANAGER_GMX_USDC_DOGE);
+        messenger.authorize(Arbitrum.X_SPOT_MANAGER_HL_USDC_DOGE);
+
+        messenger.updateGasStation(Arbitrum.GAS_STATION);
+
+        messenger.registerDstMessenger(
+            BSC_CHAIN_ID, BSC_EID, AddressCast.addressToBytes32(BscAddresses.LOGARITHM_MESSENGER)
+        );
+
+        messenger.registerStargate(ArbiAddresses.USDC, ArbiAddresses.STARGATE_POOL_USDC);
+    }
+}
+
+contract BscConfigScript is Script {
+    // access control addresses
+    address constant owner = 0xDaFed9a0A40f810FCb5C3dfCD0cB3486036414eb;
+    uint256 constant ARB_CHAIN_ID = 42161;
+    uint32 constant ARB_EID = uint32(30110);
+    ILogarithmMessenger messenger = ILogarithmMessenger(BscAddresses.LOGARITHM_MESSENGER);
+
+    function run() public {
+        vm.startBroadcast();
+
+        messenger.authorize(Bsc.BROTHER_SWAPPER_GMX);
+        messenger.authorize(Bsc.BROTHER_SWAPPER_HL);
+
+        messenger.updateGasStation(Bsc.GAS_STATION);
+
+        messenger.registerDstMessenger(
+            ARB_CHAIN_ID, ARB_EID, AddressCast.addressToBytes32(ArbiAddresses.LOGARITHM_MESSENGER)
+        );
+
+        messenger.registerStargate(BscAddresses.USDC, BscAddresses.STARGATE_POOL_USDC);
+    }
+}
