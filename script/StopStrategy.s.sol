4def4ddd571e5f3d6b320234f3569505adacb236
TalCrypto <leehunter936@gmail.com>
2024-12-23 22:39:24 +0800

    deploy: upgrade swapper on bsc

--- script/StopStrategy.s.sol
+++ script/StopStrategy.s.sol
@@ -3,13 +3,14 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
 import {BasisStrategy} from "src/strategy/BasisStrategy.sol";
+import {Arbitrum} from "script/utils/ProtocolAddresses.sol";
 
 contract StopStrategyScript is Script {
-    BasisStrategy gmxStrategy = BasisStrategy(0x166350f9b64ED99B2Aa92413A773aDCEDa1E1438);
-    BasisStrategy hlStrategy = BasisStrategy(0x6f4C89Ab99Cf5f8EA938D8899a8B1bC99a8656e4);
+    BasisStrategy gmxStrategy = BasisStrategy(Arbitrum.STRATEGY_GMX_USDC_DOGE);
+    BasisStrategy hlStrategy = BasisStrategy(Arbitrum.STRATEGY_HL_USDC_DOGE);
 
     function run() public {
         vm.startBroadcast();
-        hlStrategy.stop();
+        hlStrategy.unpause();
     }
 }
