471b1bc384a24b34fa67a02929bd3693cf1d4efc
TalCrypto <leehunter936@gmail.com>
2025-03-17 19:54:22 +0800

    feat: enable performance fee logic

--- src/vault/LogarithmVault.sol
+++ src/vault/LogarithmVault.sol
@@ -765,25 +765,14 @@ contract LogarithmVault is Initializable, PausableUpgradeable, ManagedVault {
     }
 
     /// @dev If there are pending withdraw requests, the deposited assets is used to process them.
-    /// And disables harvesting of the performance fee as a core logarithm vault doesn't have the functionality.
     ///
     /// @inheritdoc ERC4626Upgradeable
     function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal virtual override {
         if (shares == 0) {
             revert Errors.ZeroShares();
         }
-        ERC4626Upgradeable._deposit(caller, receiver, assets, shares);
+        super._deposit(caller, receiver, assets, shares);
         processPendingWithdrawRequests();
-    }
-
-    /// @dev Disables harvesting of the performance fee as a core logarithm vault doesn't have the functionality.
-    ///
-    /// @inheritdoc ERC4626Upgradeable
-    function _withdraw(address caller, address receiver, address owner, uint256 assets, uint256 shares)
-        internal
-        override
-    {
-        ERC4626Upgradeable._withdraw(caller, receiver, owner, assets, shares);
     }
 
     /*//////////////////////////////////////////////////////////////
