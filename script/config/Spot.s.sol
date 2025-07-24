558ea8c0834437f34f8636583a1e184a9c8531d5
TalCrypto <leehunter936@gmail.com>
2025-07-24 23:43:03 +0800

    chore: upgrade script

---
+++ script/config/Spot.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import {SpotManager} from "src/spot/SpotManager.sol";
+import {Arb, Bsc, Base} from "script/utils/ProtocolAddresses.sol";
+
+contract UpgradeSpotManager is Script {
+    function run() public {
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(privateKey);
+        UpgradeableBeacon beacon = UpgradeableBeacon(Arb.BEACON_SPOT_MANAGER);
+        beacon.upgradeTo(address(new SpotManager()));
+        vm.stopBroadcast();
+    }
+}
--- test/mock/UsdcMock.sol
+++ test/mock/UsdcMock.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract UsdcMock is ERC20Mock {
