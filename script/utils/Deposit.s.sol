4a0be524d5b3b52046c3bd5b4337c84bfb4a7066
Numa <numapumpilius@proton.me>
2025-07-26 02:02:50 +0300

    redeployment

--- script/utils/Deposit.s.sol
+++ script/utils/Deposit.s.sol
@@ -11,12 +11,34 @@ import {IERC20} from "forge-std/interfaces/IERC20.sol";
 contract DepositScript is Script {
     function run() public {
         vm.startBroadcast();
-        address vault = Arb.VAULT_HL_USDC_LINK;
+        address vault = Arb.VAULT_HL_USDC_VIRTUAL;
         address asset = ArbAddresses.USDC;
-        address depositor = 0xdAC79C91164127b1E48e674dB631AfEdcc57486A;
+        address depositor = 0xF27cAf44644a4c774CDB2e6acC786c6B0fCB8dB2;
         uint256 amount = IERC20(asset).balanceOf(depositor);
         IERC20(asset).approve(vault, amount);
         LogarithmVault(vault).deposit(amount, depositor);
+        vm.stopBroadcast();
+
+        uint256 shares = IERC20(vault).balanceOf(depositor);
+        console.log("Shares: %s", vm.toString(shares));
+    }
+}
+
+contract WithdrawScript is Script {
+    function run() public {
+        vm.startBroadcast();
+        address vault = Arb.VAULT_HL_USDC_VIRTUAL;
+        address asset = ArbAddresses.USDC;
+        address depositor = 0xF27cAf44644a4c774CDB2e6acC786c6B0fCB8dB2;
+        uint256 idleAssets = LogarithmVault(Arb.VAULT_HL_USDC_VIRTUAL).idleAssets();
+        console.log("idleAssets", vm.toString(idleAssets));
+
+        uint256 balBefore = IERC20(asset).balanceOf(depositor);
+        console.log("balBefore", vm.toString(balBefore));
+        IERC20(vault).approve(vault, type(uint256).max);
+        LogarithmVault(vault).withdraw(idleAssets, depositor, depositor);
+        uint256 balAfter = IERC20(asset).balanceOf(depositor);
+        console.log("balAfter", vm.toString(balAfter));
         vm.stopBroadcast();
 
         uint256 shares = IERC20(vault).balanceOf(depositor);
