9909812a3391dcdb7a9ba92861d3d4d9f0b8980f
TalCrypto <leehunter936@gmail.com>
2025-09-02 00:11:39 +0800

    feat: isClaimed

--- src/vault/LogarithmVault.sol
+++ src/vault/LogarithmVault.sol
@@ -600,6 +600,13 @@ contract LogarithmVault is Initializable, PausableUpgradeable, ManagedVault {
         return isExecuted && !withdrawRequest.isClaimed;
     }
 
+    /// @notice Tells if the withdraw request is claimed or not.
+    function isClaimed(bytes32 withdrawRequestKey) external view returns (bool) {
+        LogarithmVaultStorage storage $ = _getLogarithmVaultStorage();
+        WithdrawRequest memory withdrawRequest = $.withdrawRequests[withdrawRequestKey];
+        return withdrawRequest.isClaimed;
+    }
+
     /// @notice Tells if the owner is prioritized to withdraw.
     function isPrioritized(address owner) public view returns (bool) {
         address _priorityProvider = _getLogarithmVaultStorage().priorityProvider;
